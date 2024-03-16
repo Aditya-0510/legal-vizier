cb906f7484e6e953a0f6501c9c38be4c747e0cea
Bhagya-vishwakarma <157792295+Bhagya-vishwakarma@users.noreply.github.com>
2024-03-16 16:40:16 +0530

    Added button for light and dark theme

--- clickcards/buttons.js
+++ clickcards/buttons.js
@@ -1,25 +1,19 @@
 
-// console.log("jhfddsd");
 
 
 imgs = document.querySelectorAll(".boximg")
-
-
-
-
-
-t = document.querySelectorAll(".boximg> .pname")
-
+images = document.querySelectorAll(".boximg>img")
+text = document.querySelectorAll(".boximg> .pname")
+t = document.querySelectorAll(".boximg> .t")
     for (let index = 0; index < imgs.length; index++) {
-            imgs[index].addEventListener("click", () => {
-
-            // t[index].style.color="#0b0c35"
-            imgs[index].style.backgroundColor="#000000";
-            // imgs[index].style.boxShadow = "-4px 0px 10px #000000"
-            
+        imgs[index].addEventListener("click", () => {
+            images[index].style.filter="brightness(0.75)";
+            images[index].style.border = "5px solid rgb(255, 198, 65)"
+            text[index].style.color="rgb(255, 198, 65)"
+            t[index].style.color="rgb(255, 198, 65)"
             setTimeout(()=>{
                 window.location.href = "http://www.google.com";
-            },300)
+            },200)
            
         })
 
--- malefemalepage/a.js
+++ malefemalepage/a.js
@@ -30,3 +30,34 @@ function change_onclick() {
 
 
 }
+
+
+
+
+// darmode button 
+
+
+function changebutbg(){
+    bool=document.getElementById("modebutton").checked
+
+    btn=document.getElementById("modebutton")
+
+    if (bool==true){
+        btn.style.backgroundColor="black"
+        document.head.innerHTML=`<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <!-- <link rel="stylesheet" href="style_dark.css"> -->
+        <link rel="stylesheet" href="style.css">`
+
+    }
+    else{
+
+        btn.style.backgroundColor="#c6c6c6"
+
+        document.head.innerHTML=`<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="style_dark.css"> 
+        <!--<link rel="stylesheet" href="style.css">-->`
+    }}
